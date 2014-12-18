3e2158900b58fc15a6c4f0525dda0ef3cc647f24
dingruiqiang <783482203@qq.com>
2014-12-18 17:32:39 +0800

    init

---
+++ WtlFramework/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MDIExSample.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MDICHILD                    129
+#define IDB_DUMMYBMP                    201
+#define IDI_TASKLIST                    202
+#define IDI_OUTPUT                      203
+#define IDI_ICON1                       204
+#define IDI_ICON2                       205
+#define IDI_ICON3                       206
+#define ID_VIEW_FOLDERS                 32772
+#define ID_VIEW_OUTPUT                  32773
+#define ID_VIEW_TASKLIST                32774
+#define ID_VIEW_COLORPENEL             32775
+#define ID_VIEW_COLORWHEEL             32776
+#define ID_VIEW_LAYERS             32777
+
+#define ID_FILE_CLOSE32778              32778
+#define ID_EDIT_IMAGE                   32779
+
+#define ID_VIEW_TOOLSETS           32780
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        207
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
